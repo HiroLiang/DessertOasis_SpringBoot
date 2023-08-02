7e0286ab7e3fa315f2ac2fe92650ccca370d9e71
HiroLiang <hiromichi.liang@gmail.com>
2023-08-03 00:42:22 +0800

    start

---
+++ src/main/java/com/dessertoasis/demo/DessertOasisApplication.java
@@ -0,0 +1,13 @@
+package com.dessertoasis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DessertOasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DessertOasisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dessertoasis/demo/DessertOasisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dessertoasis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DessertOasisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
