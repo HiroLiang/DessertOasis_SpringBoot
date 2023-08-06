f2c71ab36d268d6264494dcbf4448e97f578266b
Imei <s83582003@gmail.com>
2023-08-06 17:45:24 +0800

    no message

--- src/main/java/com/dessertoasis/demo/model/recipe/RecipeRepository.java
+++ src/main/java/com/dessertoasis/demo/model/recipe/RecipeRepository.java
@@ -8,12 +8,15 @@ import org.springframework.data.repository.query.Param;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
 	
+	//食譜名稱模糊搜尋
 	@Query("from Recipe where recipeTitle like %?1% ")
 	List<Recipe> findRecipeByTiltleLike(String recipeTitle);
 	
+	//食譜分類搜尋
 	@Query("from Recipe where categoryID = :cid")
 	List<Recipe> findRecipeByCategoryID(@Param("cid") String categoryID);
 	
+	//食譜難易度搜尋
 	@Query("from Recipe where difficulty = :dif")
 	List<Recipe> findRecipeByDifficulty(@Param("dif") String difficulty);
 	
