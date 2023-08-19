d213e5b587b7506f0c004f25818f027d12538a25
LAPTOP-9B2P3HMH\tsai <hero929408@gmail.com>
2023-08-19 15:53:41 +0800

    新增 CorsCofig

---
+++ src/main/java/com/dessertoasis/demo/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.dessertoasis.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**") // project 中所有 api 都支持跨域
+		        .allowedOrigins("http://localhost:8081/", "http://localhost:5173/") // 設置放行哪些原始域
+//		        .allowCredentials(true) // 是否發送 Cookie
+		        .allowedMethods("*") // 放行哪些請求方法 GET, POST, PUT, DELETE,...
+//		        .maxAge(3600) // 跨域允許時間
+		        ;
+	}
+	
+}
