52749fa81857d10f4011dc411e59c75802c37252
HiroLiang <hiromichi.liang@gmail.com>
2023-08-21 00:28:39 +0800

    test postreq to server

--- src/main/java/com/dessertoasis/demo/controller/Test.java
+++ src/main/java/com/dessertoasis/demo/controller/Test.java
@@ -1,16 +1,20 @@
 package com.dessertoasis.demo.controller;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Test {
 	
-	@GetMapping("/test")
-	public ResponseEntity<String> test(){
-
+	@PostMapping("/test")
+	public ResponseEntity<String> test(@RequestBody Map<String, Object> reqbody){
+		String name = (String) reqbody.get("name");
+		System.out.println(name);
 		return new ResponseEntity<>("Hello world", HttpStatus.CREATED);
 	}
 
