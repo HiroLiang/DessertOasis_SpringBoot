80119f5c1bbaabbd9fdd76a8224e18e7bbc7c6a8
HiroLiang <hiromichi.liang@gmail.com>
2023-08-20 23:54:30 +0800

    no message

--- src/main/java/com/dessertoasis/demo/controller/Test.java
+++ src/main/java/com/dessertoasis/demo/controller/Test.java
@@ -2,10 +2,10 @@ package com.dessertoasis.demo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class Test {
 	
 	@GetMapping("/test")
