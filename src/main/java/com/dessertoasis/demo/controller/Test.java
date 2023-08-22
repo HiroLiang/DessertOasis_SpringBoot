c10b7cd3de939e00eec0b7bbba7455d72e426dfd
HiroLiang <hiromichi.liang@gmail.com>
2023-08-22 09:15:16 +0800

    no message

--- src/main/java/com/dessertoasis/demo/controller/Test.java
+++ src/main/java/com/dessertoasis/demo/controller/Test.java
@@ -1,19 +1,19 @@
 package com.dessertoasis.demo.controller;
 
-import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 public class Test {
 	
 	@PostMapping("/test")
-	public ResponseEntity<String> test(@RequestBody Map<String, Object> reqbody){
-		String name = (String) reqbody.get("name");
+	public ResponseEntity<String> test(HttpSession session){
+		session.setAttribute("name", "john");
+		String name = (String) session.getAttribute("name");
 		System.out.println(name);
 		return new ResponseEntity<>("Hello world", HttpStatus.CREATED);
 	}
