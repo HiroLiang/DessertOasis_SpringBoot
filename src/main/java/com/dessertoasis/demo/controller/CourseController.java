08f15f6ff063a79bc626697a4c58300cb8ed1971
hhc93 <clarahc30@gmail.com>
2023-08-24 11:05:51 +0800

    修改controller

--- src/main/java/com/dessertoasis/demo/controller/CourseController.java
+++ src/main/java/com/dessertoasis/demo/controller/CourseController.java
@@ -9,12 +9,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dessertoasis.demo.model.course.Course;
 import com.dessertoasis.demo.model.course.CourseRepository;
 import com.dessertoasis.demo.service.CourseService;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 
 @RestController
@@ -68,4 +71,41 @@ public class CourseController {
 		}
 		return "刪除失敗";
 	}
+	
+	@PostMapping("/checkTeacher")
+    @ResponseBody
+	public String checkTeacher(HttpServletRequest request,@RequestBody Course course) {
+		// 獲取請求中的所有Cookie
+        Cookie[] cookies = request.getCookies();
+        
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+            	System.out.println(cookie.getName());
+            	if(cookie.getName().equals("login1")&& cookie.getValue().equals("true")) {
+            		System.out.println(course.getTeacherId());
+            		if(course.getTeacherId()== 2) {
+//            			System.out.println(course.getTeacherId());
+            			return "這是 Teacher id";
+            		}
+            		return "這不是 Teacher id";
+            	}
+//                // 检查Cookie的名称是否为"teacher"，这里假设教师信息存储在名为"teacher"的Cookie中
+//                if ("teacherId".equals(cookie.getName())) {
+//                    // 如果找到名为"teacher"的Cookie，編寫逻辑判断教师信息是否存在于Cookie中
+//                    // 比如，檢查Cookie的值是否符合教師的條件
+//                    String teacherInfo = cookie.getValue();
+//                    if (isTeacherId(teacherInfo)) {
+//                        return "这是一个教师Cookie";
+//                    }
+//                }
+            }
+        }return "这不是一个教师Cookie";
+	}
+	
+//	private boolean isTeacherId(String cookieValue) {
+//        // 编写逻辑来判断Cookie中的值是否表示教师
+//        // 您可以根据您的需求定义教师信息的格式和判断条件
+//        // 这里只是一个示例，您可能需要根据实际情况进行修改
+//        return cookieValue != null && cookieValue.contains("teacherId");
+//    }
 }
