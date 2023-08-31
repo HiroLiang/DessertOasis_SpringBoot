388ccd27a38e8f0e54270d8b8e6b676c07062f33
hhc93 <clarahc30@gmail.com>
2023-08-31 14:30:04 +0800

    no message

--- src/main/java/com/dessertoasis/demo/model/course/Course.java
+++ src/main/java/com/dessertoasis/demo/model/course/Course.java
@@ -1,8 +1,11 @@
 package com.dessertoasis.demo.model.course;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
+
+import com.dessertoasis.demo.model.recipe.RecipeCategory;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -12,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -59,9 +63,13 @@ public class Course {
 	@Column(name="courseLocation",columnDefinition = "nvarchar(150)")
 	private String courseLocation;
 	
-	//課程分類編號
-	@Column(name="courseCategoryId",nullable = false,columnDefinition = "int")
-	private Integer courseSortId;
+//	//課程分類編號
+//	@Column(name="courseCategoryId",nullable = false,columnDefinition = "int")
+//	private Integer courseSortId;
+	
+	//分類Id  OneToMany
+	@OneToMany(mappedBy = "course")
+	private List<CourseCategory> courseCategories;
 	
 	//剩餘名額
 	@Column(name="remainingPlaces",columnDefinition = "int")
