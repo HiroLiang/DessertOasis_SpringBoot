cfc35d95e0f39e366208001f8dc081d5595d49a1
HiroLiang <hiromichi.liang@gmail.com>
2023-09-01 18:14:12 +0800

    新增 動態搜尋範例

--- src/main/java/com/dessertoasis/demo/controller/TeacherController.java
+++ src/main/java/com/dessertoasis/demo/controller/TeacherController.java
@@ -1,16 +1,14 @@
 package com.dessertoasis.demo.controller;
 
-
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dessertoasis.demo.model.sort.DateRules;
-import com.dessertoasis.demo.model.sort.SearchRules;
+import com.dessertoasis.demo.model.sort.SortCondition;
 import com.dessertoasis.demo.service.TeacherService;
 
 import jakarta.servlet.http.Cookie;
@@ -22,16 +20,15 @@ public class TeacherController {
 
 	@Autowired
 	private TeacherService tService;
-	
-	@GetMapping
-	public String getTeacherDatas(Integer page, Integer pageSize , String[] sortRule,List<DateRules> dateRules,List<SearchRules> searchRules,String[] numberRange) {
-		tService.getTeacherPage(page, pageSize, sortRule, dateRules, searchRules, numberRange);
-		
+
+	@PostMapping("/getTeacherPage")
+	public String getTeacherDatas(@RequestBody SortCondition sortCod) {
+		tService.getTeacherPage(sortCod);
+
 		return null;
 	}
 
-	
-	//依照老師編號列出該教師所有課程
+	// 依照老師編號列出該教師所有課程
 //	@GetMapping("/teacher/{teacherId}/courses")
 //    public List<Course> getTeacherCourses(@PathVariable Integer teacherId) {
 //		Teacher teacher = tRepo.findByTeacherId(teacherId);
@@ -40,35 +37,35 @@ public class TeacherController {
 //        }
 //        return Collections.emptyList();
 //    }
-	
+
 	@GetMapping("/check-teacher")
-    public ResponseEntity<String> checkTeacher(HttpServletRequest request) {
-        // 從Cookie中獲得身份訊息
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if ("userType".equals(cookie.getName()) && "teacher".equals(cookie.getValue())) {
-                    // 是教師身分
-                    return ResponseEntity.ok("You are a teacher.");
-                }
-            }
-        }
-        
-        // 不是教師身分
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied.");
-    }
-	
+	public ResponseEntity<String> checkTeacher(HttpServletRequest request) {
+		// 從Cookie中獲得身份訊息
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if ("userType".equals(cookie.getName()) && "teacher".equals(cookie.getValue())) {
+					// 是教師身分
+					return ResponseEntity.ok("You are a teacher.");
+				}
+			}
+		}
+
+		// 不是教師身分
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied.");
+	}
+
 	@GetMapping("/set-teacher-cookie")
-    public ResponseEntity<String> setTeacherCookie(HttpServletResponse response) {
-        // 創建一個名為"userType"的Cookie，並將值設置為"teacher"
-        Cookie userTypeCookie = new Cookie("userType", "teacher");
+	public ResponseEntity<String> setTeacherCookie(HttpServletResponse response) {
+		// 創建一個名為"userType"的Cookie，並將值設置為"teacher"
+		Cookie userTypeCookie = new Cookie("userType", "teacher");
 
-        // 可以設置Cookie的其他屬性，如過期時間、域等
-        // userTypeCookie.setMaxAge(3600); // 過期时間設為1小時
+		// 可以設置Cookie的其他屬性，如過期時間、域等
+		// userTypeCookie.setMaxAge(3600); // 過期时間設為1小時
 
-        // 添加Cookie到response
-        response.addCookie(userTypeCookie);
+		// 添加Cookie到response
+		response.addCookie(userTypeCookie);
 
-        return ResponseEntity.ok("Teacher cookie set successfully.");
-    }
+		return ResponseEntity.ok("Teacher cookie set successfully.");
+	}
 }
--- src/main/java/com/dessertoasis/demo/service/TeacherService.java
+++ src/main/java/com/dessertoasis/demo/service/TeacherService.java
@@ -1,20 +1,26 @@
 package com.dessertoasis.demo.service;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.dessertoasis.demo.model.course.Teacher;
 import com.dessertoasis.demo.model.course.TeacherRepository;
 import com.dessertoasis.demo.model.member.Member;
 import com.dessertoasis.demo.model.sort.DateRules;
 import com.dessertoasis.demo.model.sort.SearchRules;
+import com.dessertoasis.demo.model.sort.SortCondition;
+import com.dessertoasis.demo.model.sort.SortCondition.SortWay;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Join;
@@ -26,30 +32,116 @@ public class TeacherService {
 
 	@Autowired
 	private TeacherRepository tRepo;
-	
+
 	@PersistenceContext
 	private EntityManager em;
-	
-	@GetMapping("/teacher/{id}/course")
+
 	public Optional<Teacher> getCourseByTeacherId(Integer id) {
-        Optional<Teacher> data = tRepo.findById(id);
-        return data;
-    }
-	
-	public String getTeacherPage(Integer page, Integer pageSize , String[] sortRule,List<DateRules> dateRules,List<SearchRules> searchRules,String[] numberRange) {
+		Optional<Teacher> data = tRepo.findById(id);
+		return data;
+	}
+
+	public String getTeacherPage(SortCondition sortCod) {
 		CriteriaBuilder cb = em.getCriteriaBuilder();
+		// 決定select table
 		CriteriaQuery<Teacher> cq = cb.createQuery(Teacher.class);
 		Root<Teacher> root = cq.from(Teacher.class);
+		// 決定join Table
 		Join<Teacher, Member> memberJoin = root.join("member");
-		cq.multiselect(root.get("id"),memberJoin.get("fullName"),root.get("teacherTel"),root.get("teacherMail"));
-		int conditionLength = dateRules.size()+searchRules.size()+numberRange.length;
-		Predicate[] predicates = new Predicate[conditionLength];
+		// 決定搜索欄位
+		cq.multiselect(root.get("id"), memberJoin.get("fullName"), root.get("teacherTel"), root.get("teacherMail"));
+		// 計算條件數量
+		List<DateRules> dateRules = sortCod.getDateRules();
+		List<SearchRules> searchRules = sortCod.getSearchRules();
+		String numKey = sortCod.getNumKey();
+		int n = 0;
+		if (dateRules != null)
+			n += sortCod.getDateRules().size();
+		if (searchRules != null)
+			n += sortCod.getSearchRules().size();
+		if (numKey != null)
+			n += 1;
+		System.out.println(n);
 		int counter = 0;
-		for (DateRules dateRule : dateRules) {
-			
+		Teacher teacher = new Teacher();
+		Member member = new Member();
+
+		// 遍歷搜索條件
+		Predicate[] predicates = new Predicate[n];
+		// 日期範圍搜索
+		if (dateRules != null) {
+			for (DateRules rule : dateRules) {
+				if (hasProperty(teacher, rule.getKey())) {
+					predicates[counter] = cb.between(root.get(rule.getKey()), rule.getStart(), rule.getEnd());
+					counter++;
+				} else if (hasProperty(member, rule.getKey())) {
+					predicates[counter] = cb.between(memberJoin.get(rule.getKey()), rule.getStart(), rule.getEnd());
+					counter++;
+				}
+			}
 		}
-		
-		
+		// 模糊搜索條件
+		if (searchRules != null) {
+			for (SearchRules rule : searchRules) {
+				if (hasProperty(teacher, rule.getKey())) {
+					predicates[counter] = cb.like(root.get(rule.getKey()), rule.getInput());
+					counter++;
+				} else if (hasProperty(member, rule.getKey())) {
+					predicates[counter] = cb.like(memberJoin.get(rule.getKey()), rule.getInput());
+					counter++;
+				}
+			}
+		}
+		// 數字範圍搜索
+		if (numKey != null) {
+			if (hasProperty(teacher, numKey)) {
+				predicates[counter] = cb.between(root.get(numKey), sortCod.getNumStart(), sortCod.getNumEnd());
+				counter++;
+			} else if (hasProperty(member, numKey)) {
+				predicates[counter] = cb.between(memberJoin.get(numKey), sortCod.getNumStart(), sortCod.getNumEnd());
+				counter++;
+			}
+		}
+		if(sortCod.getSortBy()!=null) {
+			if(hasProperty(teacher, sortCod.getSortBy())) {
+				if(sortCod.getSortWay().equals(SortWay.ASC)) {
+					cq.where(predicates).orderBy(cb.asc(root.get(sortCod.getSortBy())));
+				}else {
+					cq.where(predicates).orderBy(cb.desc(root.get(sortCod.getSortBy())));
+				}
+			}else if(hasProperty(member, sortCod.getSortBy())) {
+				if(sortCod.getSortWay().equals(SortWay.ASC)) {
+					cq.where(predicates).orderBy(cb.asc(memberJoin.get(sortCod.getSortBy())));
+				}else {
+					cq.where(predicates).orderBy(cb.desc(memberJoin.get(sortCod.getSortBy())));
+				}
+			}else {
+				cq.where(predicates);
+			}
+		}else {
+			cq.where(predicates);
+		}
+		TypedQuery<Teacher> typedQuery = em.createQuery(cq);
+		typedQuery.setFirstResult((sortCod.getPage() - 1) * sortCod.getPageSize());
+		typedQuery.setMaxResults(sortCod.getPageSize());
+		List<Teacher> result = typedQuery.getResultList();
+		System.out.println(result);
 		return null;
+	}
+
+	public static boolean hasProperty(Object bean, String propertyName) {
+		try {
+			BeanInfo beanInfo = Introspector.getBeanInfo(bean.getClass());
+			PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+
+			for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+				if (propertyDescriptor.getName().equals(propertyName)) {
+					return true;
+				}
+			}
+		} catch (IntrospectionException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
