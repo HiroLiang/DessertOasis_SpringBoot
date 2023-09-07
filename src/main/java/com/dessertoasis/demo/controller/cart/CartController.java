2a763487c980221f4baf494011a64b0dd0d880d5
LAPTOP-9B2P3HMH\tsai <hero929408@gmail.com>
2023-09-07 23:15:02 +0800

    修改 cart controller

--- src/main/java/com/dessertoasis/demo/controller/cart/CartController.java
+++ src/main/java/com/dessertoasis/demo/controller/cart/CartController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -60,6 +61,11 @@ public class CartController {
 		cartService.insert(cartDTO, member.getId());
 		
 		return "加入購物車成功";
+	}
+	
+	//@Patch
+	public List<ProductCartDTO> updateProdQuantities(@RequestBody List<ProductCartDTO> productCartDTOs) {
+		return null;
 	}
 	
 	// 刪除購物車項目
