f39a43ddc8adb226732a75958616bf64b8347f9a
Han <roiipjoi@gmail.com>
2023-09-11 07:28:47 +0800

    cookies的AES加密解密

--- src/main/java/com/dessertoasis/demo/config/SecurityConfig.java
+++ src/main/java/com/dessertoasis/demo/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.dessertoasis.demo.config;
 
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,5 +17,12 @@ public class SecurityConfig {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Bean //創建了一個用於 AES 加密和解密的金鑰對象
+    public SecretKey secretKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128); 
+        return keyGenerator.generateKey();
+    }
 
 }
