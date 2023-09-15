65b55a1cc37cfd78aaf0747117404136ffb8693f
hhc93 <clarahc30@gmail.com>
2023-09-15 13:59:39 +0800

    修改關聯

--- src/main/java/com/dessertoasis/demo/model/course/Teacher.java
+++ src/main/java/com/dessertoasis/demo/model/course/Teacher.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.dessertoasis.demo.model.member.Member;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -50,13 +51,13 @@ public class Teacher {
 	private String teacherAccountStatus;
 
 	//會員ID (FK)，要有對方的物件當屬性
-	@Column(name = "memberId", nullable = false, insertable = false, updatable = false)
-	private Integer memberId;
+//	@Column(name = "memberId", nullable = false, insertable = false, updatable = false)
+//	private Integer memberId;
 	
 	//一個老師有一個會員id，用JsonIgnore避免無窮迴圈
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "memberId")
-	@JsonIgnore
+	@JsonIgnoreProperties({"teacher"})
 	private Member member;
 
 	// 一個教師有多堂課(一對多)
@@ -74,21 +75,23 @@ public class Teacher {
 
 	public Teacher() {
 	}
+	
+	
 
-	public Teacher(Integer id, Integer memberId, String teacherContract, String teacherName, String teacherProfilePic,
-			Integer teacherTel, String teacherMail, String teacherProfile, String teacherAccountStatus, Member member) {
-		super();
-		this.id = id;
-		this.memberId = memberId;
-		this.teacherContract = teacherContract;
-		this.teacherName = teacherName;
-		this.teacherProfilePic = teacherProfilePic;
-		this.teacherTel = teacherTel;
-		this.teacherMail = teacherMail;
-		this.teacherProfile = teacherProfile;
-		this.teacherAccountStatus = teacherAccountStatus;
-		this.member = member;
-	}
+//	public Teacher(Integer id, Integer memberId, String teacherContract, String teacherName, String teacherProfilePic,
+//			Integer teacherTel, String teacherMail, String teacherProfile, String teacherAccountStatus, Member member) {
+//		super();
+//		this.id = id;
+//		this.memberId = memberId;
+//		this.teacherContract = teacherContract;
+//		this.teacherName = teacherName;
+//		this.teacherProfilePic = teacherProfilePic;
+//		this.teacherTel = teacherTel;
+//		this.teacherMail = teacherMail;
+//		this.teacherProfile = teacherProfile;
+//		this.teacherAccountStatus = teacherAccountStatus;
+//		this.member = member;
+//	}
 
 	public Teacher(Integer id,String fullName, Integer teacherTel, String teacherMail) {
 		super();
@@ -97,5 +100,24 @@ public class Teacher {
 		this.teacherMail = teacherMail;
 		this.member.setFullName(fullName);
 	}
+
+
+
+public Teacher(Integer id, String teacherName, String teacherProfilePic, String teacherContract, Integer teacherTel,
+		String teacherMail, String teacherProfile, String teacherAccountStatus, Member member, List<Course> courseList,
+		List<TeacherExpertise> expertiseList) {
+	super();
+	this.id = id;
+	this.teacherName = teacherName;
+	this.teacherProfilePic = teacherProfilePic;
+	this.teacherContract = teacherContract;
+	this.teacherTel = teacherTel;
+	this.teacherMail = teacherMail;
+	this.teacherProfile = teacherProfile;
+	this.teacherAccountStatus = teacherAccountStatus;
+	this.member = member;
+	this.courseList = courseList;
+	this.expertiseList = expertiseList;
+}
 
 }
