fb0c04297455cd2e867c05c20756220d1c2552aa
Han <roiipjoi@gmail.com>
2023-09-18 05:20:21 +0800

    member圖片

--- src/main/java/com/dessertoasis/demo/controller/member/MemberController.java
+++ src/main/java/com/dessertoasis/demo/controller/member/MemberController.java
@@ -3,6 +3,9 @@ package com.dessertoasis.demo.controller.member;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Base64;
@@ -13,7 +16,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +27,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -29,7 +35,9 @@ import com.dessertoasis.demo.ImageUploadUtil;
 import com.dessertoasis.demo.model.member.Member;
 import com.dessertoasis.demo.model.member.MemberAccess;
 import com.dessertoasis.demo.model.member.MemberDetail;
+import com.dessertoasis.demo.model.member.MemberDetailRepository;
 import com.dessertoasis.demo.model.recipe.PicturesDTO;
+import com.dessertoasis.demo.model.recipe.Recipes;
 import com.dessertoasis.demo.service.member.MemberDetailService;
 import com.dessertoasis.demo.service.member.MemberService;
 
@@ -42,8 +50,14 @@ public class MemberController {
 	@Autowired
 	private MemberService mService;
 
+	private MemberDetailRepository mRepo;
+	
 	@Autowired
 	private MemberDetailService mdService;
+	
+	@Autowired
+	private ImageUploadUtil imgUtil;
+
 
 	// 多筆
 	@GetMapping("/all")
@@ -145,54 +159,115 @@ public class MemberController {
 		}
 	}
 	
+	//處理圖片路近並儲存圖片路徑
 	@PostMapping("/uploadMemberImg")
-	public String memberPic(@RequestBody List<PicturesDTO> pictures,HttpSession session){
-		
-		Member member = (Member) session.getAttribute("loggedInMember");
-		if (member != null) {
-			final String uploadPath = "D:/dessertoasis-vue/public/";
-			if (!pictures.isEmpty() && pictures != null) {
-				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS");
-				String timestamp = LocalDateTime.now().format(formatter);
-				
-				Integer memId = member.getId();
-				
-				String sqlPath = "images/member" + "/" + memId + "/";
-				String userFolder = uploadPath + sqlPath;
-	
-				File folder = new File(userFolder);
-				if (!folder.exists()) {
-					folder.mkdirs();
-				}
-				if (pictures.get(0) != null) {
-					/*----------取得前端傳來的圖檔字串----------*/
-					String mainPic = pictures.get(0).getBase64Content();
-					String mainPicName = pictures.get(0).getFileName();
-					/*----------取得前端傳來的圖檔字串----------*/
+	public String memberPic(@RequestBody List<PicturesDTO> pictures, HttpSession session) {
+	    Member member = (Member) session.getAttribute("loggedInMember");
+	    if (member != null) {
+	        final String uploadPath = "C:/dessertoasis-vue/public/";
+	        if (!pictures.isEmpty()) {
+	            try {
+	                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS");
+	                String timestamp = LocalDateTime.now().format(formatter);
 
-					/*----------寫入檔案及儲存位置串接字串----------*/
-					String mainFileName = mainPicName.substring(0, mainPicName.lastIndexOf("."));// 將副檔名與檔名拆開 取得檔名
-					String mainExtension = mainPicName.substring(mainPicName.lastIndexOf("."));// 將副檔名與檔名拆開 取得副檔名
-					String mainUniqueName = mainFileName + "_" + timestamp + mainExtension;// 將時間串入檔名
+	                Integer memId = member.getId();
 
-					byte[] mainDecode = Base64.getDecoder().decode(mainPic);
-					File mainfile = new File(userFolder + mainUniqueName);
-					
-					try (FileOutputStream mainfileOutputStream = new FileOutputStream(mainfile)) {
-						mainfileOutputStream.write(mainDecode);
-						mainfileOutputStream.flush();
-						
-						return userFolder;
-						
-					} catch (IOException e) {
-						e.printStackTrace();
-						System.out.println(e);
-					}
-			}
-			
-		}
-		
+	                String sqlPath = "images/member" + "/" + memId + "/";
+	                String userFolder = uploadPath + sqlPath;
+
+	                File folder = new File(userFolder);
+	                if (!folder.exists()) {
+	                    folder.mkdirs();
+	                }
+
+	                if (!pictures.get(0).getBase64Content().isEmpty()) {
+	                    /*----------取得前端傳來的圖檔字串----------*/
+	                    String mainPic = pictures.get(0).getBase64Content();
+	                    String mainPicName = pictures.get(0).getFileName();
+	                    /*----------取得前端傳來的圖檔字串----------*/
+//	                    System.out.println("mainPicName: " + mainPicName);
+	                    /*----------寫入檔案及儲存位置串接字串----------*/
+	                    String mainFileName = mainPicName.substring(0, mainPicName.lastIndexOf("."));// 將副檔名與檔名拆開 取得檔名
+	                    String mainExtension = mainPicName.substring(mainPicName.lastIndexOf("."));// 將副檔名與檔名拆開 取得副檔名
+	                    String mainUniqueName = mainFileName + "_" + timestamp + mainExtension;// 將時間串入檔名
+
+//	                    System.out.println("mainFileName: " + mainFileName);
+//	                    System.out.println("mainExtension: " + mainExtension);
+//	                    System.out.println("timestamp: " + timestamp);
+//	                    
+	                    byte[] mainDecode = Base64.getDecoder().decode(mainPic);
+	                    File mainfile = new File(userFolder + mainUniqueName);
+
+	                    try (FileOutputStream mainfileOutputStream = new FileOutputStream(mainfile)) {
+	                        mainfileOutputStream.write(mainDecode);
+	                        mainfileOutputStream.flush();
+	                        String imgUrl = sqlPath+mainPicName;
+	                        
+	                        MemberDetail memberDetail = member.getMemberDetail();
+	                        if (memberDetail != null) {
+	                           //儲存圖片路徑
+	                            memberDetail.setPic(imgUrl);
+
+	                          
+	                            mdService.updateMemberDetail(memId, memberDetail);
+	                        }
+	                        return imgUrl;
+
+	                    } catch (IOException e) {
+	                        e.printStackTrace();
+	                        System.out.println(e);
+	                        return "寫入失敗";
+	                    }
+	                }
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	                System.out.println(e);
+	            }
+	        }
+	    }
+
+	    return "未成功";
 	}
-		return null;
-}
+	
+	//獲取圖片URL
+	 @GetMapping("/getImageURL")
+	    public ResponseEntity<String> getImageURL(HttpSession session) {
+	        Member member = (Member) session.getAttribute("loggedInMember");
+	        if (member != null) {
+	            MemberDetail memberDetail = member.getMemberDetail();
+	            if (memberDetail != null) {
+	                String imageURL = memberDetail.getPic();
+	                if (imageURL != null && !imageURL.isEmpty()) {
+	                    return ResponseEntity.ok().body(imageURL);
+	                }
+	            }
+	        }
+	        return ResponseEntity.notFound().build();
+	    }
+	
+//	 @GetMapping("/getMemberPic")
+//	    public ResponseEntity<byte[]> getMemberPic(HttpSession session) {
+//	        Member member = (Member) session.getAttribute("loggedInMember");
+//	        if (member != null) {
+//	            MemberDetail memberDetail = member.getMemberDetail();
+//	            if (memberDetail != null && memberDetail.getPic() != null) {
+//	                try {
+//	                    // 从文件系统或其他存储位置读取图片内容
+//	                    Path imagePath = Paths.get("C:/dessertoasis-vue/public/", memberDetail.getPic());
+//	                    byte[] imageBytes = Files.readAllBytes(imagePath);
+//
+//	                    HttpHeaders headers = new HttpHeaders();
+//	                    headers.setContentType(MediaType.IMAGE_JPEG); // 设置响应的Content-Type
+//
+//	                    return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+//	                } catch (IOException e) {
+//	                    e.printStackTrace();
+//	                    return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+//	                }
+//	            }
+//	        }
+//	        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//	    }
+
+
 }
