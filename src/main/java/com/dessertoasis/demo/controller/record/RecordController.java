68495be0011e3f49ef96f94fb1822b3c7955f3df
HiroLiang <hiromichi.liang@gmail.com>
2023-09-20 17:17:10 +0800

    no message

--- src/main/java/com/dessertoasis/demo/controller/record/RecordController.java
+++ src/main/java/com/dessertoasis/demo/controller/record/RecordController.java
@@ -1,5 +1,30 @@
 package com.dessertoasis.demo.controller.record;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import com.dessertoasis.demo.model.record.Record;
+import com.dessertoasis.demo.service.record.RecordService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
 public class RecordController {
 
+	@Autowired
+	private RecordService rService;
+
+	@PutMapping("/record/set")
+	public void setRecord(Record record, HttpSession session) {
+		Integer categoryId = record.getCategoryId();
+		switch (categoryId) {
+		case 1:
+
+			Record setRecord = rService.setRecord(record);
+			if (setRecord != null) {
+
+			}
+		}
+	}
 }
